2fd50f583330c06f1d156a25a1590163c71eb4d1
rengstrom <engstrom.robin@gmail.com>
2026-02-26 18:07:07 +0100

    Decomp 5 functions in 1C38.c (async load helpers + init caller)
    - func_80011B60: async load from D_80097410 to 0x80097940
    - func_80011C68: async load from D_80097400 to 0x80098000
    - func_80011CB0: async load from D_80097808 to 0x801B0000 + DrawSync
    - func_80011D0C: calls func_80011C68, func_80098390, func_80028444
    - func_80011E18: async load from D_80051694 to 0x80097400

--- src/1C38.c
+++ src/1C38.c
@@ -20,19 +20,50 @@ INCLUDE_ASM("asm/nonmatchings/1C38", func_80011A60);
 
 INCLUDE_ASM("asm/nonmatchings/1C38", func_80011AE0);
 
-INCLUDE_ASM("asm/nonmatchings/1C38", func_80011B60);
+extern u32 D_80097410[];
+
+void func_80011B60(void) {
+    func_8003882C(D_80097410[2], D_80097410[3], 0x80097940, 0);
+    while (func_800393C8() != 0)
+        ;
+}
 
 INCLUDE_ASM("asm/nonmatchings/1C38", func_80011BA8);
 
-INCLUDE_ASM("asm/nonmatchings/1C38", func_80011C68);
+extern u32 D_80097400[];
 
-INCLUDE_ASM("asm/nonmatchings/1C38", func_80011CB0);
+void func_80011C68(void) {
+    func_8003882C(D_80097400[0], D_80097400[1], 0x80098000, 0);
+    while (func_800393C8() != 0)
+        ;
+}
 
-INCLUDE_ASM("asm/nonmatchings/1C38", func_80011D0C);
+extern u32 D_80097808[];
+
+void func_80011CB0(void) {
+    func_8003882C(D_80097808[0], D_80097808[1], 0x801B0000, 0);
+    while (func_800393C8() != 0)
+        ;
+    func_8002C3AC(0x801B0000, 0);
+    while (DrawSync(1) != 0)
+        ;
+}
+
+void func_80011D0C(void) {
+    func_80011C68();
+    func_80098390();
+    func_80028444();
+}
 
 INCLUDE_ASM("asm/nonmatchings/1C38", func_80011D3C);
 
-INCLUDE_ASM("asm/nonmatchings/1C38", func_80011E18);
+extern u32 D_80051694[];
+
+void func_80011E18(void) {
+    func_8003882C(D_80051694[0], D_80051694[1], 0x80097400, 0);
+    while (func_800393C8() != 0)
+        ;
+}
 
 INCLUDE_ASM("asm/nonmatchings/1C38", main);
 
